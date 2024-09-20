ce84d04b5fce850944a523a27deea63aab6b592b
RatMonkey2000 <sam.soonawalla@gmail.com>
2024-09-20 10:52:58 +0100

    Done

---
+++ Euler 41-60/Euler57.py
@@ -0,0 +1,21 @@
+from fractions import Fraction
+def isTopHeavy(numK):
+    frac=Fraction(numK)
+    print(frac)
+    if len(str(frac[0]))>len(str(frac[1])):
+        return True
+    return False
+
+def iter(numJ):
+    totNum=0
+    i=0
+    while i<10:
+        numJ+=1
+        numJ=1+1/numJ
+        print(numJ)
+        if isTopHeavy(numJ):
+            totNum+=1
+        i+=1
+    return(totNum)
+
+print(iter(1))
