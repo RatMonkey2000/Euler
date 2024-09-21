052e21533c2e20a84498950c327709d593bb9eed
RatMonkey2000 <sam.soonawalla@gmail.com>
2024-09-21 13:52:58 +0100

    Done

--- Euler 41-60/Euler57.py
+++ Euler 41-60/Euler57.py
@@ -1,18 +1,29 @@
 from fractions import Fraction
+
+def strToList(string):
+    place=string.find("/")
+    List=[]
+    List.append(string[:place])
+    List.append(string[place+1:])
+    return(List)
+
 def isTopHeavy(numK):
+    print(numK)
     frac=Fraction(numK)
+    frac=str(frac)
+    frac=strToList(frac)
     print(frac)
     if len(str(frac[0]))>len(str(frac[1])):
+        print(frac)
         return True
     return False
 
 def iter(numJ):
     totNum=0
     i=0
-    while i<10:
+    while i<200:
         numJ+=1
         numJ=1+1/numJ
-        print(numJ)
         if isTopHeavy(numJ):
             totNum+=1
         i+=1
