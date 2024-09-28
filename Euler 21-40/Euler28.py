b3751e1e171e23b2fd480f39601e8cd56b126538
RatMonkey2000 <sam.soonawalla@gmail.com>
2024-09-28 14:15:12 +0100

    Done

--- Euler 21-40/Euler28.py
+++ Euler 21-40/Euler28.py
@@ -4,13 +4,9 @@ Sum=1
 count=1
 while count<=500:
 	Sum+=prev+add
-	print(prev+add)
 	Sum+=prev+add*2
-	print(prev+add*2)
 	Sum+=prev+add*3
-	print(prev+add*3)
 	Sum+=prev+add*4
-	print(prev+add*4)
 	prev+=add*4
 	add+=2
 	count+=1
