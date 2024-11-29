7523a463eb814c0bc6a20b388b6eeed73a6992ee
Sam Soonawalla <sam.soonawalla@gmail.com>
2024-11-29 10:54:04 +0000

    Done

---
+++ Euler 61-80/Euler61.py
@@ -0,0 +1,32 @@
+from math import sqrt
+def isTrianglular(numJ):
+    numJ=-1/2+sqrt(2*numJ+1/4)
+    if numJ%1==0:
+        return True
+    return False
+
+def isSquare(numJ):
+    if sqrt(numJ)%1==0:
+        return True
+    return False
+
+def isPentagonal(numJ):
+    numJ=1/6+sqrt(2*numJ/3+1/36)
+    if numJ%1==0:
+        return True
+    return False
+
+def isHexagonal(numJ):
+    numJ=1/4+sqrt(numJ/2+1/16)
+    if numJ%1==0:
+        return True
+    return False
+
+def isHeptagonal(numJ):
+    numJ=3/10+sqrt(2*numJ/5+9/100)
+    if numJ%1==0:
+        return True
+    return False
+
+def isOctagonal(numJ):
+    
