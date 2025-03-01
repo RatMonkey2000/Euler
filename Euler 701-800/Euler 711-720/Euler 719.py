5fb47e99c313ae5af9e197c6e802f97072fcb0f1
Sam Soonawalla <sam.soonawalla@gmail.com>
2025-03-01 12:45:53 +0000

    Done

--- Euler 701-800/Euler 711-720/Euler 719.py
+++ Euler 701-800/Euler 711-720/Euler 719.py
@@ -1,12 +1,11 @@
 from math import sqrt
-num=101
-Sum=41333
 
 def listWorks(listi):
     i=0
     sumNum=0
     while i<len(listi):
-        sumNum+=int(listi[i])
+        if listi[i]!=None:
+            sumNum+=int(listi[i])
         i+=1
     return sumNum
 
@@ -16,25 +15,36 @@ def nextPerm(listi):
         if len(listi[i])!=1:
             if listi[i+1]==None:
                 listi[i+1]=listi[i][-1]
+                listi[i]=listi[i][:-1]
             else:
-                listi[i+1]+=listi[i][-1]
+                listi[i+1]=listi[i][-1]+listi[i+1]
+                listi[i]=listi[i][:-1]
             return listi
         else:
             i+=1
     return False
 
 def isnatural(num):
-    i=0
+    i=1
     splits=[]
+    splits.append(str(num))
     while i<len(str(num)):
         splits.append(None)
         i+=1
-    splits.append(str(num))
-    while not listWorks(listi):
-        listi=nextPerm(listi)
-    if listi==False:
-        return False
-    else:
+    done=False
+    while splits[-1]==None and not done:
+        if listWorks(splits)==sqrt(num):
+            done=True
+        splits=nextPerm(splits)
+    if done:
         return True
-
-print(isnatural(7))
+    return False
+    
+num=101
+Sum=41333
+while num<1000001:
+    if num%10000==0:
+        print(num/10000)
+    if isnatural(num**2):
+        Sum+=num**2
+    num+=1
