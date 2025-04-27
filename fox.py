f25b5532c76c932ffc6293df05629e8ca32e0659
Sam Soonawalla <sam.soonawalla@gmail.com>
2025-04-27 13:11:32 +0100

    Done

---
+++ fox.py
@@ -0,0 +1,96 @@
+def isWrong():
+  if (fox==chicken and chicken!=farmer) or (chicken==grain and grain!=farmer):
+    return True
+  return False
+def state():
+  print("On Wrong side of River:")
+  if fox==0:
+    print("fox")
+  if chicken==0:
+    print("chicken")
+  if grain==0:
+    print("grain")
+  if farmer==0:
+    print("farmer")
+  print("On Right Side")
+  if fox==1:
+    print("fox")
+  if chicken==1:
+    print("chicken")
+  if grain==1:
+    print("grain")
+  if farmer==1:
+    print("farmer")
+def isCorrect():
+  if fox==1 and chicken==1 and grain==1 and farmer==1:
+    return True
+  return False
+  
+
+print("""
+A fox, chicken and a bag of grain wait by the side of a river.
+Which item will you take in your rowboat to the other side?
+fox, chicken, grain or farmer?""")
+choise=input("Choose:")
+fox=0
+chicken=0
+grain=0
+farmer=0
+if choise=="Farmer" or choise=="farmer" or choise=="FARMER":
+  farmer=(farmer+1)%2
+elif choise=="Fox" or choise=="fox" or choise=="FOX":
+  if fox==farmer:
+    farmer=(farmer+1)%2
+    fox=(fox+1)%2
+  else:
+    print("Invalid! Start again!")
+    exit()
+elif choise=="Chicken" or choise=="chicken" or choise=="CHICKEN":
+  if chicken==farmer:
+    farmer=(farmer+1)%2
+    chicken=(chicken)%2
+  else:
+    print("Invalid! Start again!")
+    exit()
+elif choise=="Grain" or choise=="grain" or choise=="GRAIN":
+  if grain==farmer:
+    farmer=(farmer+1)%2
+    grain=(grain+1)%2
+  else:
+    print("Invalid! Start again!")
+    exit()
+else:
+  print("Invalid! Start again!")
+  exit()
+print(isCorrect())
+while not isCorrect:
+  state()
+  choise=input("Choose:")
+  if choise=="Farmer" or choise=="farmer" or choise=="FARMER":
+    farmer=(farmer+1)%2
+  elif choise=="Fox" or choise=="fox" or choise=="FOX":
+    if fox==farmer:
+      farmer=(farmer+1)%2
+      fox=(fox+1)%2
+    else:
+      print("Invalid! Start again!")
+      exit()
+  elif choise=="Chicken" or choise=="chicken" or choise=="CHICKEN":
+    if chicken==farmer:
+      farmer=(farmer+1)%2
+      chicken=(chicken)%2
+    else:
+      print("Invalid! Start again!")
+      exit()
+  elif choise=="Grain" or choise=="grain" or choise=="GRAIN":
+    if grain==farmer:
+      farmer=(farmer+1)%2
+      grain=(grain+1)%2
+    else:
+      print("Invalid! Start again!")
+      exit()
+  
+
+  
+
+  
